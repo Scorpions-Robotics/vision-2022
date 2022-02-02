2969604f5e309eba13bb72bfd5d1fe839e923c8c
egeakman <egeakmanegeakman@hotmail.com>
2022-02-02 20:13:11 +0300

    Remove unnecessary parantheses

--- misc/flask/flask_server.py
+++ misc/flask/flask_server.py
@@ -23,7 +23,7 @@ def gen_frames():
         stream = cv2.imdecode(npimg, 1)
         buffer = cv2.imencode(".jpg", stream)[1]
         frame = buffer.tobytes()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+        yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
 
 
 @app.route("/")
