7cbdeb4131c05fa417a027c03d84ec615ef04ef3
Arda Sak <ardasak434@gmail.com>
2022-02-04 15:51:58 +0300

    Tests updae

--- misc/tests/test_networktables.py
+++ misc/tests/test_networktables.py
@@ -6,28 +6,30 @@ from misc.functions import network
 
 
 table = network.nt_listener_init()
+ball_table = table.getSubTable("ball")
+hoop_table = table.getSubTable("hoop")
 
 
-def try_get_string(key):
+def try_get_string(table, key):
     return table.getString(key, "")
 
 
-def try_get_number(key):
+def try_get_number(table, key):
     return table.getNumber(key, 0)
 
 
 while True:
     try:
-        x = try_get_string("X")
-        y = try_get_string("Y")
-        w = try_get_string("W")
-        h = try_get_string("H")
-        d = try_get_string("D")
-        r = try_get_string("R")
-        b = try_get_string("B")
-        alliance = try_get_string("alliance")
-        period = try_get_string("period")
-        position = try_get_number("position")
+        x = try_get_string(ball_table, "X")
+        y = try_get_string(ball_table, "Y")
+        w = try_get_string(ball_table, "W")
+        h = try_get_string(ball_table, "H")
+        d = try_get_string(ball_table, "D")
+        r = try_get_string(ball_table, "R")
+        b = try_get_string(ball_table, "B")
+        alliance = try_get_string(ball_table, "alliance")
+        period = try_get_string(ball_table, "period")
+        position = try_get_number(ball_table, "position")
 
         print(
             f"X: {x} Y: {y} W: {w} H: {h} D: {d} R: {r} B: {b} alliance: {alliance} period: {period} position: {position}"
